d68dc8e0dc344c624ec411e18d8d786549afce16
quadros2 <quadros2@gmail.com>
2020-01-17 11:04:25 -0600

    inflation commit

--- src/main/java/com/example/mydebtapp1/DebtsFragment.java
+++ src/main/java/com/example/mydebtapp1/DebtsFragment.java
@@ -8,8 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 //import com.example.junk.R;
 
@@ -21,7 +30,9 @@ public class DebtsFragment extends Fragment {
 
     Button add;
 
-    LinearLayout addedDebt;
+    ScrollView addedDebt;
+
+    DebtClassToPush debtClassToPush;
 
     public DebtsFragment() {
         // Required empty public constructor
@@ -35,8 +46,32 @@ public class DebtsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_debts, container, false);
 
         addedDebt = rootView.findViewById(R.id.MyDebts);
-        View debtChunk = getLayoutInflater().inflate(R.layout.chunk_your_debt, addedDebt, false);
 
+        FirebaseDatabase.getInstance().getReference().child("User")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("My Debt")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            View debtChunk = getLayoutInflater().inflate(R.layout.chunk_your_debt, addedDebt, false);
+                            debtClassToPush = snapshot.getValue(DebtClassToPush.class);
+                            TextView recipient = debtChunk.findViewById(R.id.NameRecipient);
+                            recipient.setText(debtClassToPush.getRecipient());
+                            TextView amount = debtChunk.findViewById(R.id.AmountStated);
+                            amount.setText(debtClassToPush.getAmount());
+                            TextView dateCreated = debtChunk.findViewById(R.id.Datecreated);
+                            dateCreated.setText(debtClassToPush.getDate());
+                            TextView description = debtChunk.findViewById(R.id.Description);
+                            description.setText(debtClassToPush.getDescription());
+                            addedDebt.addView(debtChunk);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
         add = rootView.findViewById(R.id.addDebts);
         add.setOnClickListener(V -> {
